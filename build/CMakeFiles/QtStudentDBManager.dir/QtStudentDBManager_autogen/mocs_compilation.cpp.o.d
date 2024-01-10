4f7f8ab2d4fafa5cb9dcebe02b83107700c38742
himcesjf <himcesjf.prog@gmail.com>
2024-01-10 06:04:32 +0530

    Remove build dir

--- build/CMakeFiles/QtStudentDBManager.dir/QtStudentDBManager_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/QtStudentDBManager.dir/QtStudentDBManager_autogen/mocs_compilation.cpp.o: \
- /home/himcesjf/Code/Acumen/Assignments/Day1/QtStudentDBManager/build/QtStudentDBManager_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/QtStudentDBManager.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for QtStudentDBManager.
-# This may be replaced when dependencies are built.
