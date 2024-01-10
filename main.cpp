a8b1a0bc331a8963058349c94572eb602e042b2e
himcesjf <himcesjf.prog@gmail.com>
2024-01-10 14:25:29 +0530

    Modified main.cpp to implement GUI functionality

--- main.cpp
+++ main.cpp
@@ -1,58 +1,26 @@
-#include <QCoreApplication>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
 #include "DatabaseOperations.h"
-#include "Student.h"
-#include <QDebug>
+#include "StudentModel.h"
 
 int main(int argc, char *argv[]) {
-    QCoreApplication a(argc, argv);
+    QGuiApplication app(argc, argv);
 
-    // Initialize and set up the database
-    DatabaseOperations dbOps;
-    dbOps.createTable();
+    QQmlApplicationEngine engine;
 
-    dbOps.deleteAllStudents();
+    StudentModel studentModel;
 
-    // Create a new student and insert it into the database
-    Student student1;
-    student1.setId(1);
-    student1.setFirstName("Himanshu");
-    student1.setLastName("Rajput");
-    student1.setMiddleName("A");
-    student1.setRoll(101);
-    student1.setClass("10A");
-    dbOps.insertStudent(student1); // Insert the student record into the database
+    // Expose StudentModel to QML
+    engine.rootContext()->setContextProperty("studentModel", &studentModel);
 
-    // Display all students
-    dbOps.displayAllStudents();
+    // Load the main QML file - BrowseStudents.qml
+    // The home screen will be the the browse students view; App starts with this view.
+    const QUrl url(QStringLiteral("qrc:/BrowseStudents.qml"));
+    engine.load(url);
 
-    //Inserting record of student
-    Student student2;
-    student2.setId(2);
-    student2.setFirstName("Hemant");
-    student2.setLastName("Roy");
-    student2.setMiddleName("B");
-    student2.setRoll(102);
-    student2.setClass("10B");
-    dbOps.insertStudent(student2);
+    if (engine.rootObjects().isEmpty())
+        return -1;
 
-    dbOps.displayAllStudents();
-
-    // Update the student's information
-    student2.setFirstName("Hemang");
-    dbOps.updateStudent(student2);
-
-    // Display all students again to see the updated information
-    dbOps.displayAllStudents();
-
-    // Delete the student from the database
-    //dbOps.deleteStudent(student.getId());
-    dbOps.deleteStudent(1);
-
-    // Display all students to confirm deletion
-    dbOps.displayAllStudents();
-
-    dbOps.closeDatabaseConnection();
-
-    //return a.exec();
-    return 0;
+    return app.exec();
 }
