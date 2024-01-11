c9b759e51f73641e176612deb5a6e8312e797a58
himcesjf <himcesjf.prog@gmail.com>
2024-01-12 02:47:46 +0530

    Remove build directory from repository

--- build/CMakeFiles/client.dir/client_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/client.dir/client_autogen/mocs_compilation.cpp.o: \
- /home/himcesjf/Code/Acumen/Assignments/Network_Minimal/build/client_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/server.dir/server_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/server.dir/server_autogen/mocs_compilation.cpp.o: \
- /home/himcesjf/Code/Acumen/Assignments/Network_Minimal/build/server_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
