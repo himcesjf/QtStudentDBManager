2f79b2c9b65403920819d610fd277b25b130e6b6
himcesjf <himcesjf.prog@gmail.com>
2024-01-14 07:04:37 +0530

    Make the server not disconnect after sending data to the client

--- main_server.cpp
+++ main_server.cpp
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
             out << *student;
         }
 
-        clientConnection->disconnectFromHost();
+        //clientConnection->disconnectFromHost();
     });
 
     int result = app.exec();
